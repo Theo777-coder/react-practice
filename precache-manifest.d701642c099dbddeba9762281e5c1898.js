7292e0e4ba5c8f1a0b230b0f92e7400a365f3374
Theo <67960721+Theo777-coder@users.noreply.github.com>
2020-08-03 23:21:09 +0100

    Updates

---
+++ precache-manifest.d701642c099dbddeba9762281e5c1898.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46be50e0a005e2a47c8d441c24e7cf1",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "3e967a2129d581d9e35b",
+    "url": "/react-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edabd2260d053d2df4d5",
+    "url": "/react-practice/static/js/2.6e836624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practice/static/js/2.6e836624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e967a2129d581d9e35b",
+    "url": "/react-practice/static/js/main.dc7ca7f2.chunk.js"
+  },
+  {
+    "revision": "08d9ce32a05402505166",
+    "url": "/react-practice/static/js/runtime-main.60d0d113.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
